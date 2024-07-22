de3ab26aad9211b8799cc43d45303f580a2fbdd9
SidharthAnand04 <sanand12@illinois.edu>
2024-07-22 15:29:42 -0700

    New file

---
+++ demo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i <= size; i++) { // Off-by-one error
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int *arr;
+    int size = 10;
+    
+    arr = (int *)malloc(size * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = i * 2;
+    }
+
+    printArray(arr, size);
+
+    free(arr);
+    arr = NULL; // Setting arr to NULL after free, good practice but often forgotten
+
+    printf("Array values after free:\n");
+    printArray(arr, size); // Using freed memory
+
+    return 0;
+}
